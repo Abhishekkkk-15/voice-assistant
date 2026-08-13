02695217553cc824c5dce21a3614f2648df6bac5
Abhishek Jangid <mrabhi748@gmail.com>
2026-08-13 16:26:03 +0530

    chore: remove unncessary files from root and chage the output save path

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ async def chat_completion(prompt:str):
     vs = VoiceAssistant()
     user_query = prompt
     llm_output = vs.ask_llm(user_query)
-    vs.text_to_speech(llm_output,output_filename="output.mp3")
+    vs.text_to_speech(llm_output,output_filename="./output/output.mp3")
     
     return FileResponse(
-        path=Path("./output.mp3"),
+        path=Path("./output/output.mp3"),
         filename="output.mp3",
         media_type="audio/mpeg"
     )
