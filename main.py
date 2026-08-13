b5ba8114930c8f43704b8f7d69254fc601eb650a
Abhishek Jangid <mrabhi748@gmail.com>
2026-08-13 16:54:39 +0530

    feat(web): add a web for testing the voice assistant and also added cors middlewares

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ from fastapi import FastAPI,Response
 from fastapi.responses import FileResponse
 from pathlib import Path
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+origins = [
+    "http://localhost:3000",    
+    "http://localhost:5173",     
+    "http://localhost:8001",     
+    "https://yourfrontend.com",  
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,           
+    allow_methods=["*"],              
+    allow_headers=["*"],              
+)
 
 @app.get("/")
 def health():
